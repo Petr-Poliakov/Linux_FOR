5ac5799b1f72eceb7c03e594c7d9bfeb3a832de7
Petr-Poliakov <94320840+Petr-Poliakov@users.noreply.github.com>
2023-07-03 23:18:51 +0300

    Add files via upload

---
+++ 7z_negativ.py
@@ -0,0 +1,17 @@
+from checkout import ssh_checkout_negative
+import yaml
+from conftest import data
+
+
+#
+def test_step8():
+    # test1
+    assert ssh_checkout_negative(data["host"], data["user"], data["password"],
+                                 "cd {}; 7z e badarx.7z -o{} -y".format(data['folder_out'], data['folder_ext']),
+                                 "ERROR"), "Test8 Fail"
+
+
+def test_step9():
+    # test2
+    assert ssh_checkout_negative(data["host"], data["user"], data["password"],
+                                 "cd {}; 7z t badarx.7z".format(data['folder_out']), "ERROR"), "Test9 Fail"
